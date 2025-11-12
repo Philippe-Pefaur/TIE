61525bce71824f4172943f94c3f070789bb24b4e
Philippe-Pefaur <phpefaur@gmail.com>
2025-11-12 20:38:37 -0300

    Agregado estilo a la página, terminado vista Diario.js.

---
+++ frontend/src/vistas/Mensual.js
@@ -0,0 +1,9 @@
+function Mensual() {
+    return(
+        <div>
+            <h1>MENSUAL</h1>
+        </div>
+    );
+}
+
+export default Mensual;
---
+++ frontend/src/vistas/Semanal.js
@@ -0,0 +1,9 @@
+function Semanal() {
+    return(
+        <div>
+            <h1>SEMANAL</h1>
+        </div>
+    );
+}
+
+export default Semanal;
