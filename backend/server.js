6f8647be8acff2e6c42d14b81a5b48e9036be132
Philippe-Pefaur <phpefaur@gmail.com>
2025-11-18 19:02:21 -0300

    Agregado funcionalidad para crear y recuperar archivos de resumen mensual para datos de consumo.

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,28 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-function agregarRegistro(fecha) { // Agrega un día al registro liviano
+function agregarRegistro(fecha, semana=false) { // Agrega un día o semana al registro liviano
     const rutaArchivo = path.join(__dirname, 'data/registro.json');
     const dia = fecha.substring(0,2);
 
     if (!fs.existsSync(rutaArchivo)) { // Si no existe el archivo de registros, crearlo
-        const result = { // Registro en JSON
-            [fecha.substring(3,10)]: [dia] // Clave: Mes, Valor: Día
-        };
-
+        let result;
+        if (!semana) {
+            result = { // Registro en JSON
+                [fecha.substring(3,10)]: { // Clave: Mes, Valor: Días y semanas
+                    dias: [dia], 
+                    semanas: []
+                }
+            };
+        } else {
+            result = { // Registro en JSON
+                [fecha.substring(3,10)]: { // Clave: Mes, Valor: Días y semanas
+                    dias: [], 
+                    semanas: [dia]
+                }
+            };
+        }
+        
         fs.writeFileSync(rutaArchivo, JSON.stringify(result, null, 2)); // Escribir el archivo
         console.log(`Dato guardado en ${rutaArchivo}`);
 
@@ -25,8 +38,18 @@ function agregarRegistro(fecha) { // Agrega un día al registro liviano
     const result = JSON.parse(contenido);
 
     if (!result[fecha.substring(3,10)]) { // Si no existe un registro para el mes
-        result[fecha.substring(3,10)] = [dia];  // Crear la clave del mes y agregar el día
-
+        if (!semana) {
+            result[fecha.substring(3,10)] = { // Crear la clave del mes y agregar el día
+                dias: [dia], 
+                semanas: []
+            };  
+        } else {
+            result[fecha.substring(3,10)] = { // Crear la clave del mes y agregar las semana
+                dias: [], 
+                semanas: [dia]
+            };  
+        }
+        
         fs.writeFileSync(rutaArchivo, JSON.stringify(result, null, 2)); // Escribir el archivo
         console.log(`Dato guardado en ${rutaArchivo}`);
 
@@ -34,8 +57,14 @@ function agregarRegistro(fecha) { // Agrega un día al registro liviano
     }
     
     // Si ya hay registros para el mes
-    if (!result[fecha.substring(3,10)].includes(dia)) { // Sólo si el día no esta registrado, agregarlo
-        result[fecha.substring(3,10)].push(dia);
+    if (!semana) {
+        if (!result[fecha.substring(3,10)].dias.includes(dia)) { // Sólo si el día no esta registrado, agregarlo
+            result[fecha.substring(3,10)].dias.push(dia);
+        }
+    } else {
+        if (!result[fecha.substring(3,10)].semanas.includes(dia)) { // Sólo si la semana no esta registrado, agregarlo
+            result[fecha.substring(3,10)].semanas.push(dia);
+        }
     }
     
     fs.writeFileSync(rutaArchivo, JSON.stringify(result, null, 2)); // Escribir el archivo
@@ -101,6 +130,52 @@ function actualizarResumenSemanal(data) { // Funcion para crear o actualizar dat
     fs.writeFileSync(rutaArchivo, JSON.stringify(result, null, 2)); // Escribir el archivo
     console.log(`Dato guardado en ${rutaArchivo}`);
 
+    agregarRegistro(semana, true);
+
+    return;
+}
+
+function actualizarResumenMensual(data) {
+    const rutaArchivo = path.join(__dirname, `data/${data.fecha.substring(3, 10)}/resumenMes.json`); // Obtener la dirección del archivo
+
+    if (!fs.existsSync(rutaArchivo)) { // Si no existe, crearlo
+        const result = {
+            fecha: data.fecha.substring(3, 10),
+            dias: [data.fecha.substring(0, 2)],
+            consumo: [data.consumo.reduce((acc, val) => acc + val, 0)],
+            generacion: [data.generacion.reduce((acc, val) => acc + val, 0)],
+            consumoSuministroGeneral: [data.consumoSuministroGeneral.reduce((acc, val) => acc + val, 0)],
+            perdida: [data.perdida.reduce((acc, val) => acc + val, 0)]
+        }
+
+        fs.writeFileSync(rutaArchivo, JSON.stringify(result, null, 2)); // Escribir el archivo
+
+        return; // Terminar el proceso
+    }
+
+    // Si el archivo ya existe
+    const contenido = fs.readFileSync(rutaArchivo, 'utf8'); // Leer el archivo
+    const result = JSON.parse(contenido);
+
+    if (result.dias.includes(data.fecha.substring(0, 2))) { // Si el día ya fué agregado
+        indice = result.dias.indexOf(data.fecha.substring(0, 2)) // Obtener el índice del día
+        
+        // Editar los consumos acumulados según el índice
+        result.consumo[indice] = data.consumo.reduce((acc, val) => acc + val, 0); 
+        result.generacion[indice] = data.generacion.reduce((acc, val) => acc + val, 0);
+        result.consumoSuministroGeneral[indice] = data.consumoSuministroGeneral.reduce((acc, val) => acc + val, 0);
+        result.perdida[indice] = data.perdida.reduce((acc, val) => acc + val, 0);
+    } else { // Si el día no ha sido agregado, agregar los datos nuevos
+        result.dias.push(data.fecha.substring(0, 2));
+        result.consumo.push(data.consumo.reduce((acc, val) => acc + val, 0));
+        result.generacion.push(data.generacion.reduce((acc, val) => acc + val, 0));
+        result.consumoSuministroGeneral.push(data.consumoSuministroGeneral.reduce((acc, val) => acc + val, 0));
+        result.perdida.push(data.perdida.reduce((acc, val) => acc + val, 0));
+    }
+
+    fs.writeFileSync(rutaArchivo, JSON.stringify(result, null, 2)); // Escribir el archivo
+    console.log(`Dato guardado en ${rutaArchivo}`);
+
     return;
 }
 
@@ -129,6 +204,7 @@ function agregarConsumo(data) { // Agregar o crear archivo de datos de consumo
 
         agregarRegistro(data.fecha); // Agregar registro del día
         actualizarResumenSemanal(result); // Actualizar resumen semanal
+        actualizarResumenMensual(result); // Actualizar resumen mensual
 
         return; // Terminar el proceso
     }
@@ -150,6 +226,7 @@ function agregarConsumo(data) { // Agregar o crear archivo de datos de consumo
 
     agregarRegistro(data.fecha); // Agregar registro del día
     actualizarResumenSemanal(result); // Actualizar resumen semanal
+    actualizarResumenMensual(result); // Actualizar resumen mensual
 
     return;
 }
@@ -192,6 +269,20 @@ app.get('/api/semanal/:fecha', (req, res) => {
     const semana = `${diaInicio}-${mesInicio}-${añoInicio}`;
 
     const rutaArchivo = path.join(__dirname, `data/${req.params.fecha.substring(3, 10)}/resumenSemana-${semana}.json`); // Obtener la dirección del archivo
+    if (!fs.existsSync(rutaArchivo)) { // Si no existe responder con error
+        res.status(400).json({message: 'Resumen semanal no encontrado'});
+    }
+
+    const content = fs.readFileSync(rutaArchivo, 'utf8'); // Cargar el archivo y enviarlo como respuesta
+    result = JSON.parse(content);
+    res.status(200).json(result);
+})
+
+app.get('/api/mensual/:fecha', (req, res) => {
+    // Obtener fecha de los datos
+    const[dia, mes, año] = req.params.fecha.split('-');
+
+    const rutaArchivo = path.join(__dirname, `data/${req.params.fecha.substring(3, 10)}/resumenMes.json`); // Obtener la dirección del archivo
     if (!fs.existsSync(rutaArchivo)) { // Si no existe responder con error
         res.status(400).json({message: 'Resumen semanal no encontrado'});
     }
